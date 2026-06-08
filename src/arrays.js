0d2fb9828e78a5c6c56ce5b79930a6ead48fece2
JhonatasPaiva <jhonatas-psn@hotmail.com>
2026-06-07 21:57:19 -0300

    first commit

---
+++ src/arrays.js
@@ -0,0 +1,12 @@
+const Aluno = [
+    {nome: 'João', nota: 7},
+    {nome: 'Maria', nota: 5},
+    {nome: 'Pedro', nota: 4},
+    {nome: 'Ana', nota: 10}
+]
+
+const aprovados = Aluno.filter(function(item){
+    return item.nota >= 6
+})
+
+console.log(aprovados)
