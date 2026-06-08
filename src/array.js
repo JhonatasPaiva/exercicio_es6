30f4e57b52b16e23231d2af2b6832ee8a3313cd1
JhonatasPaiva <jhonatas-psn@hotmail.com>
2026-06-07 22:04:51 -0300

    adicionando objeto

--- src/array.js
+++ src/array.js
@@ -1,11 +1,11 @@
-const Aluno = [
+const Alunos = [
     {nome: 'João', nota: 7},
     {nome: 'Maria', nota: 5},
     {nome: 'Pedro', nota: 4},
     {nome: 'Ana', nota: 10}
 ]
 
-const aprovados = Aluno.filter(function(item){
+const aprovados = Alunos.filter(function(item){
     return item.nota >= 6
 })
 
